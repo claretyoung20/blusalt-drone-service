11e85586b39031517dd69be38ccbf0cb8d9a137b
Young Maryclaret Nnenna <claretyoung@gmail.com>
2023-04-04 18:07:30 +0100

    remove cron job as the implementation is not maintainable

--- src/main/java/com/blusalt/droneservice/DroneServiceApplication.java
+++ src/main/java/com/blusalt/droneservice/DroneServiceApplication.java
@@ -2,12 +2,10 @@ package com.blusalt.droneservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
-@EnableScheduling
 public class DroneServiceApplication {
 
 	public static void main(String[] args) {
